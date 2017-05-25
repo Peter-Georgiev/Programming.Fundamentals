2a2760770878e42a802b4ea3d6f13308cd830d86
Peter Georgiev <peroxmen@hotmail.com>
2017-05-25 16:55:31 +0300

    New project - C# Basic Syntax - More Exercises

---
+++ BasicSyntax-MoreExercises/01.X/X.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+    class X
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int leftAndRight = 0;
+        int center = n;
+                
+        for (int i = 0; i < n / 2; i++)
+        {
+            center = n - leftAndRight - 2;
+            Console.Write(new string(' ', leftAndRight / 2) + "x");
+            Console.Write(new string(' ', center) + "x");
+            Console.WriteLine(new string(' ', leftAndRight / 2));
+            leftAndRight += 2;
+        }
+
+        Console.WriteLine(new string(' ', n / 2) + "x" + new string(' ', n / 2));
+
+        leftAndRight = n - 2;
+        center = 1;
+
+        for (int i = 0; i < n / 2; i++)
+        {
+            
+            Console.Write(new string(' ', leftAndRight / 2) + "x");
+            Console.WriteLine(new string(' ', center) + "x");
+            leftAndRight -= 2;
+            center += 2;
+        }  
+    }
+}
---
+++ BasicSyntax-MoreExercises/02.VaporStore/VaporStore.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+class VaporStore
+{
+    static void Main()
+    {
+        double balance = double.Parse(Console.ReadLine());
+        string line;
+        string messages;
+        double price = 0.00d;
+        double pay = balance;
+        bool isExitPay = true;
+
+        while (isExitPay)
+        {
+            line = Console.ReadLine();
+            isExitPay = !line.Equals("Game Time");
+            if (isExitPay)
+            {
+                switch (line)
+                {
+                    case "OutFall 4":
+                        price = 39.99d;
+                        messages = "Bought OutFall 4";
+                        break;
+                    case "CS: OG":
+                        price = 15.99d;
+                        messages = "Bought CS: OG";
+                        break;
+                    case "Zplinter Zell":
+                        price = 19.99d;
+                        messages = "Bought Zplinter Zell";
+                        break;
+                    case "Honored 2":
+                        price = 59.99d;
+                        messages = "Bought Honored 2";
+                        break;
+                    case "RoverWatch":
+                        price = 29.99d;
+                        messages = "Bought RoverWatch";
+                        break;
+                    case "RoverWatch Origins Edition":
+                        price = 39.99d;
+                        messages = "Bought RoverWatch Origins Edition";
+                        break;
+                    default:
+                        messages = "Not Found";
+                        break;
+                }
+
+                pay -= price;
+                if (pay > balance && pay > 0)
+                {
+                    messages = "Too Expensive";
+                }
+                else if (pay <= 0)
+                {
+                    messages = "Out of money!";
+                    isExitPay = false;
+                }
+
+                Console.WriteLine(messages);
+            }
+        }
+
+        if (pay > 0)
+        {
+
+            Console.WriteLine($"Total spent: $. Remaining: ${pay:F2}");
+        }
+    }
+}
