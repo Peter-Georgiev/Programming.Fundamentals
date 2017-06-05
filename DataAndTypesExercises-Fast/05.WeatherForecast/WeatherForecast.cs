20911031314c39a36c8baacadd9278f290b3b1e6
Peter Georgiev <peroxmen@hotmail.com>
2017-06-05 16:40:53 +0300

    added DataAndTypesExercises-Fast/05.WeatherForecast/WeatherForecast.cs

---
+++ DataAndTypesExercises-Fast/05.WeatherForecast/WeatherForecast.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class WeatherForecast
+{
+    static void Main()
+    {
+        try
+        {
+            long n = long.Parse(Console.ReadLine());
+
+            string message = string.Empty;
+
+            if (sbyte.MinValue <= n && n <= sbyte.MaxValue || byte.MinValue <= n && n <= byte.MaxValue)
+            {
+                message = "Sunny";
+            }
+            else if (int.MinValue <= n && n <= int.MaxValue)
+            {
+                message = "Cloudy";
+            }
+            else if (long.MinValue <= n && n <= long.MaxValue)
+            {
+                message = "Windy";
+            }
+
+            Console.WriteLine(message);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Rainy");
+        }        
+    }
+}
---
+++ DataAndTypesExercises-Fast/06.CatchTheThief/CatchTheThief.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+class CatchTheThief
+{
+    static void Main()
+    {
+        string message = Console.ReadLine();
+        int n = int.Parse(Console.ReadLine());
+        long[] array = new long[n];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = long.Parse(Console.ReadLine());
+        }
+
+        long result = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] >= n)
+            {
+                result = array[i];
+                break;
+            }
+        }
+
+        
+        for (int i = 0; i < array.Length; i++)
+        {
+
+            if (int.MaxValue <= array[i] && array[i] <= long.MaxValue && message.Equals("long"))
+            {
+                result = array[i];
+                break;
+            }
+            else if (sbyte.MaxValue <= array[i] && array[i] <= int.MaxValue && message.Equals("int"))
+            {
+                result = array[i];
+                break;
+            }
+            else if (sbyte.MinValue <= array[i] && array[i] <= sbyte.MaxValue && message.Equals("sbyte"))
+            {
+                result = array[i];
+                break;
+            }
+
+        }       
+
+        Console.WriteLine(result);
+    }
+}
