41712e79116c504f0c3615786cf571c7ecaee75f
Peter Georgiev <peroxmen@hotmail.com>
2017-06-06 17:00:05 +0300

    Added 02.AppendLists.cs.

---
+++ Lists - Lab/02.AppendLists/AppendLists.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class AppendLists
+{
+    static void Main()
+    {
+        string[] blackList = Console.ReadLine().Split(' ').ToArray();
+        List<string> fileNames = new List<string>();
+        List<string> trackDownload = new List<string>();
+        string inputFileName = Console.ReadLine();
+        
+        while (!inputFileName.Equals("end"))
+        {
+            fileNames.Add(inputFileName);
+
+            for (int i = 0; i < fileNames.Count; i++)
+            {
+                string tempfileName = fileNames[i];
+                if (tempfileName.Contains(blackList[0]))
+                {
+                    trackDownload.Add(fileNames[i]);
+                }
+            }
+
+
+            inputFileName = Console.ReadLine();
+        }
+
+        foreach (var print in trackDownload)
+        {
+            Console.WriteLine(print);
+        }
+
+        //Console.WriteLine(string.Join(" ", fileNames));
+    }
+
+}
