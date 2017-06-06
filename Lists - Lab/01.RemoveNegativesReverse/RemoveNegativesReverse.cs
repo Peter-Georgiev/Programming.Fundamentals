d648e1d8f1de62c15206bd47a38bc62859ef052c
Peter Georgiev <peroxmen@hotmail.com>
2017-06-06 16:37:22 +0300

    Added new project Lists - Lab.

---
+++ Lists - Lab/01.RemoveNegativesReverse/RemoveNegativesReverse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class RemoveNegativesReverse
+{
+    static void Main()
+    {
+        List<int> input = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+        input.RemoveAll(i => i < 0);
+        
+        if (input.Count == 0)
+        {
+            Console.WriteLine("empty");
+        }
+        else
+        {
+            input.Reverse();
+            Console.WriteLine(string.Join(" ", input));
+        }
+    }
+}
