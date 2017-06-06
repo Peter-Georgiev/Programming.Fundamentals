c519c8caae999f05d97e7b03a2470826cc146498
Peter Georgiev <peroxmen@hotmail.com>
2017-06-06 21:40:11 +0300

    Fix big

--- Lists - Lab/02.AppendLists/AppendLists.cs
+++ Lists - Lab/02.AppendLists/AppendLists.cs
@@ -6,31 +6,14 @@ class AppendLists
 {
     static void Main()
     {
+        string input = Console.ReadLine();
         string[] blackList = Console.ReadLine().Split(' ').ToArray();
         List<string> fileNames = new List<string>();
         List<string> trackDownload = new List<string>();
-        string inputFileName = Console.ReadLine();
-        
-        while (!inputFileName.Equals("end"))
+
+        for (int i = input.Length; i >= 0; i--)
         {
-            fileNames.Add(inputFileName);
 
-            for (int i = 0; i < fileNames.Count; i++)
-            {
-                string tempfileName = fileNames[i];
-                if (tempfileName.Contains(blackList[0]))
-                {
-                    trackDownload.Add(fileNames[i]);
-                }
-            }
-
-
-            inputFileName = Console.ReadLine();
-        }
-
-        foreach (var print in trackDownload)
-        {
-            Console.WriteLine(print);
         }
 
         //Console.WriteLine(string.Join(" ", fileNames));
