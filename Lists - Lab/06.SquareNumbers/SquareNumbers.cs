1a7b4b8fdb8943c68d6a31efe3c230cb06113d02
Peter Georgiev <peroxmen@hotmail.com>
2017-06-07 19:39:49 +0300

    Added Lists - Lab/06.SquareNumbers/SquareNumbers.cs and Lists - Lab/07.CountNumbers/CountNumbers.cs.

--- Lists - Lab/06.SquareNumbers/SquareNumbers.cs
+++ Lists - Lab/06.SquareNumbers/SquareNumbers.cs
@@ -6,6 +6,23 @@ class SquareNumbers
 {
     static void Main()
     {
+        int[] squareNums = Console.ReadLine()
+            .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
 
+        List<int> printSquareNums = new List<int>(squareNums.Length);
+
+        for (int i = 0; i < squareNums.Length; i++)
+        {
+            if ((int)Math.Sqrt(squareNums[i]) == (double)Math.Sqrt(squareNums[i]))
+            {
+                printSquareNums.Add(squareNums[i]);
+            }
+        }
+
+        printSquareNums.Sort((a, b) => b.CompareTo(a));
+
+        Console.WriteLine(string.Join(" ", printSquareNums));
     }
 }
---
+++ Lists - Lab/07.CountNumbers/CountNumbers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class CountNumbers
+{
+    static void Main()
+    {
+
+    }
+}
