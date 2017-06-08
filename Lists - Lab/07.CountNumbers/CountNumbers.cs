0543a33c0a6d7135c14cfd754b10227d6e00b311
Peter Georgiev <peroxmen@hotmail.com>
2017-06-08 13:29:36 +0300

    Added new Lists - Lab/07.CountNumbers/CountNumbers.cs.

--- Lists - Lab/07.CountNumbers/CountNumbers.cs
+++ Lists - Lab/07.CountNumbers/CountNumbers.cs
@@ -6,6 +6,34 @@ class CountNumbers
 {
     static void Main()
     {
+        List<int> countInput = new List<int>(1001);
+        countInput = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
 
+        Dictionary<int, int> countPrint = new Dictionary<int, int>();        
+
+        for (int i = 0; i < countInput.Count; i++)
+        {
+            byte count = 0;
+            for (int j = 0; j < countInput.Count; j++)
+            {
+                if ((int)countInput[i] == (int)countInput[j])
+                {
+                    count++;
+                }
+            }            
+
+            if (!countPrint.ContainsKey(countInput[i]))
+            {
+                countPrint.Add(countInput[i], count);
+            }
+        }
+
+        List<int> listSort = countPrint.Keys.ToList();
+        listSort.Sort();
+
+        foreach (var key in listSort)
+        {
+            Console.WriteLine($"{key} -> {countPrint[key]}");
+        }
     }
 }
