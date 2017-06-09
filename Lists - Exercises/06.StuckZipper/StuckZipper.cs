68fb8c47881723f13715b9571b27b09014849f61
Peter Georgiev <peroxmen@hotmail.com>
2017-06-09 16:55:15 +0300

    Added new project Lists - Exercises/06.StuckZipper/StuckZipper.cs.

--- Lists - Exercises/06.StuckZipper/StuckZipper.cs
+++ Lists - Exercises/06.StuckZipper/StuckZipper.cs
@@ -1,9 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class StuckZipper
 {
     static void Main()
     {
+        int[] firstLine = ReadLine();
+        int[] secondLine = ReadLine();
 
+        List<int> zipping = ZippingLine(firstLine, secondLine);
+
+        Console.WriteLine(string.Join(" ", zipping));
+    }
+
+    private static int[] ReadLine()
+    {
+        int[] readLine = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+        return readLine;
+    }
+
+    private static List<int> ZippingLine(int[] firstLine, int[] secondLine)
+    {
+        List<int> zippingLine = new List<int>();
+        byte countFirstLine = 0;
+        byte countSecondLine = 0;
+
+        for (int i = 0; i < firstLine.Length + secondLine.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                zippingLine.Add(secondLine[countSecondLine]);
+                countSecondLine++;
+            }
+            else
+            {
+                zippingLine.Add(firstLine[countFirstLine]);
+                countFirstLine++;
+            }
+        }
+
+        return zippingLine;
     }
 }
