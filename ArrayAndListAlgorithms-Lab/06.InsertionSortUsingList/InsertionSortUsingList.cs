c539d9b2ec28092bb73f9f22bbed6ad0626cf236
Peter Georgiev <peroxmen@hotmail.com>
2017-06-15 20:41:06 +0300

    Added ArrayAndListAlgorithms-Lab/06.InsertionSortUsingList/InsertionSortUsingList.cs.

--- ArrayAndListAlgorithms-Lab/06.InsertionSortUsingList/InsertionSortUsingList.cs
+++ ArrayAndListAlgorithms-Lab/06.InsertionSortUsingList/InsertionSortUsingList.cs
@@ -9,8 +9,7 @@ class InsertionSortUsingList
             .Split(' ')
             .Select(int.Parse)
             .ToArray();
-        int[] printLine = new int[readLine.Length];
-
+        
         for (int i = 1; i < readLine.Length; i++)
         {
             var current = readLine[i];
@@ -19,15 +18,14 @@ class InsertionSortUsingList
             while (previous >= 0 && readLine[previous] > current)
             {
                 readLine[previous + 1] = readLine[previous];
-                printLine[previous + 1] = readLine[previous];
-
                 previous--;
             }
 
             readLine[previous + 1] = current;
-            printLine[previous + 1] = current;
         }
 
+        int[] printLine = readLine;
+        
         Console.WriteLine(string.Join(" ", printLine));
     }
 }
