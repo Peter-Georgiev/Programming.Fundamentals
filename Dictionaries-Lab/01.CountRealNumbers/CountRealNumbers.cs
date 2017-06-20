ef3d8d166e390799eb7b8faecbd48deeda7bf828
Peter Georgiev <peroxmen@hotmail.com>
2017-06-21 00:04:38 +0300

    Added Dictionaries-Lab/01.CountRealNumbers/CountRealNumbers.cs and Dictionaries-Lab/02.OddOccurrences/OddOccurrences.cs.

---
+++ Dictionaries-Lab/01.CountRealNumbers/CountRealNumbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class CountRealNumbers
+{
+    static void Main()
+    {
+        List<double> readLine = Console.ReadLine()
+            .Split(' ')
+            .Select(double.Parse)
+            .ToList();
+
+        SortedDictionary<double, int> printResult = new SortedDictionary<double, int>();
+
+        foreach (var print in readLine)
+        {
+            if (!printResult.ContainsKey(print))
+            {
+                printResult[print] = 0;
+            }
+
+            printResult[print]++;
+        }
+
+        foreach (var print in printResult)
+        {
+            Console.WriteLine($"{print.Key} -> {print.Value}");
+        }
+    }
+}
---
+++ Dictionaries-Lab/02.OddOccurrences/OddOccurrences.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class OddOccurrences
+{
+    static void Main()
+    {
+        var words = Console.ReadLine()
+            .ToLower()
+            .Split(new[] { ' ' },
+            StringSplitOptions.RemoveEmptyEntries);
+
+        var result = new Dictionary<string, int>();
+
+        foreach (var word in words)
+        {
+            if (!result.ContainsKey(word))
+            {
+                result[word] = 0;
+            }
+
+            result[word]++;
+        }
+
+        var oddNumberWords = new List<string>();
+
+        foreach (var kvp in result)
+        {
+            var value = kvp.Value;
+
+            if (value % 2 != 0)
+            {
+                oddNumberWords.Add(kvp.Key);
+            }
+        }
+
+        Console.WriteLine(string.Join(", ", oddNumberWords));
+    }
+}
