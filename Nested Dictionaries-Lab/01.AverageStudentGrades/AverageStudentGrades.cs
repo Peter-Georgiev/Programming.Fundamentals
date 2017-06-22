3ddcf8ad8685c83faa0a1c067264707ff2e021fc
Peter-Georgiev <peter.g.georgiev@gmail.com>
2017-06-22 14:33:26 +0300

    Added Nested Dictionaries-Lab/01.AverageStudentGrades/AverageStudentGrades.cs, Nested Dictionaries-Lab/02.CitiesByContinentAndCountry/CitiesByContinentAndCountry.cs, Nested Dictionaries-Lab/03.RecordUniqueNames/RecordUniqueNames.cs and Nested Dictionaries-Lab/10. Programming-Fundamentals-Advanced-Collections-Lab.docx.

---
+++ Nested Dictionaries-Lab/01.AverageStudentGrades/AverageStudentGrades.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class AverageStudentGrades
+{
+    static void Main()
+    {
+        var studentBook = new Dictionary<string, List<double>>();
+        
+
+        var readLine = Console.ReadLine();
+        byte.TryParse(readLine, out byte count);
+
+        for (int i = 0; i < count; i++)
+        {
+            readLine = Console.ReadLine();
+            var tokens = readLine.Split(' ').ToArray();
+            var name = tokens[0];
+            var grade = double.Parse(tokens[1]);
+
+            if (!studentBook.ContainsKey(name))
+            {
+                studentBook.Add(name, new List<double>() { grade });
+            }
+            else
+            {
+                studentBook[name].Add(grade);
+            }
+        }
+
+        foreach (var pair in studentBook)
+        {
+            Console.Write($"{pair.Key} -> ");
+
+            foreach (var studentGrades in pair.Value)
+            {
+                Console.Write($"{studentGrades:F2} ");
+            }
+
+            Console.WriteLine($"(avg: {pair.Value.Average():F2})");
+        }
+    }
+}
---
+++ Nested Dictionaries-Lab/02.CitiesByContinentAndCountry/CitiesByContinentAndCountry.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class CitiesByContinentAndCountry
+{
+    static void Main()
+    {
+        var continentsData = new Dictionary<string, Dictionary<string, List<string>>>();
+
+        byte countInput  = byte.Parse(Console.ReadLine());
+
+        for (int i = 0; i < countInput; i++)
+        {           
+            var tokens = Console.ReadLine().Split(' ').ToArray();
+            var continent = tokens[0];
+            var contry = tokens[1];
+            var city = tokens[2];
+
+            if (!continentsData.ContainsKey(continent))
+            {
+                continentsData.Add(continent, new Dictionary<string, List<string>>());
+            }
+
+            if (!continentsData[continent].ContainsKey(contry))
+            {
+                continentsData[continent][contry] = new List<string>();
+                continentsData[continent][contry].Add(city);
+            }
+            else
+            {
+                continentsData[continent][contry].Add(city);
+            }           
+        }
+
+        foreach (var printContinents in continentsData)
+        {
+            var countryName = printContinents.Value;
+
+            Console.WriteLine($"{printContinents.Key}:");
+
+            foreach (var printContry in countryName)
+            {
+                var cityName = printContry.Value;
+
+                Console.Write($"{printContry.Key} -> ");
+
+                Console.WriteLine(string.Join(", ", cityName));
+            }
+        }
+    }
+}
