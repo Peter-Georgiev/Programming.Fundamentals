99384cdf3fbf7b520fe57c56303f0fd3b67a8abf
Peter Georgiev <peroxmen@hotmail.com>
2017-06-27 23:41:39 +0300

    Added LambdaAndLINQ-Exercises/02.DefaultValues/DefaultValues.cs.

--- LambdaAndLINQ-Exercises/02.DefaultValues/DefaultValues.cs
+++ LambdaAndLINQ-Exercises/02.DefaultValues/DefaultValues.cs
@@ -1,14 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 class DefaultValues
 {
     static void Main()
     {
+        var dictionary = new Dictionary<string, string>();
 
+        string readLine = String.Empty;
 
+        while (true)
+        {
+            readLine = Console.ReadLine();
+
+            if (readLine.Equals("end"))
+            {
+                break;
+            }
+            else
+            {
+                string[] input = readLine
+                .Split(new char[] { '-', '>', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+                dictionary[input[0]] = input[1];
+            }
+        }
+
+        readLine = Console.ReadLine();
+
+        //--------------------Option two--------------------//
+        PrintMethodOne(dictionary, readLine);
+
+        //--------------------Option two--------------------//
+        //PrintMethodTwo(dictionary, readLine);
+    }
+
+    private static void PrintMethodTwo(Dictionary<string, string> dictionary, string readLine)
+    {
+        var printValue = dictionary
+            .Where(x => x.Value != "null")
+            .OrderByDescending(x => x.Value.Length)
+            .ToDictionary(x => x.Key, x => x.Value);
+
+        foreach (var printNoNull in printValue)
+        {
+            Console.WriteLine($"{printNoNull.Key} <-> {printNoNull.Value}");
+        }
+
+        printValue.Clear();
+
+        printValue = dictionary
+            .Where(x => x.Value.Equals("null"))
+            .ToDictionary(x => x.Key, x => readLine);
+
+        foreach (var printNull in printValue)
+        {
+            Console.WriteLine($"{printNull.Key} <-> {printNull.Value}");
+        }
+    }
+
+    private static void PrintMethodOne(Dictionary<string, string> dictionary, string readLine)
+    {
+        dictionary
+            .Where(x => !x.Value.Equals("null"))
+            .OrderByDescending(x => x.Value.Length)
+            .ToList()
+            .ForEach(x => Console.WriteLine("{0} <-> {1}", x.Key, x.Value));
+
+        dictionary
+            .Where(x => x.Value.Equals("null"))
+            .Select(x => x.Key + " <-> " + readLine)
+            .ToList()
+            .ForEach(Console.WriteLine);
     }
 }
---
+++ LambdaAndLINQ-Exercises/03.ShoppingSpree/ShoppingSpree.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class ShoppingSpree
+{
+    static void Main()
+    {
+        Dictionary<string, Dictionary<string, string>> flattenDictionary =
+            new Dictionary<string, Dictionary<string, string>>();
+
+        while (true)
+        {
+            string readLine = Console.ReadLine();
+
+            if (readLine.ToLower().Equals("end"))
+            {
+                break;
+            }
+
+            string[] input = readLine
+                .Split(' ')
+                .ToArray();
+
+            if (!input[0].Equals("flatten"))
+            {
+                string key = input[0];
+                string innerKeys = input[1];
+                string innerValue = input[2];
+
+                if (!flattenDictionary.ContainsKey(key))
+                {
+                    flattenDictionary.Add(key, new Dictionary<string, string>());
+                    flattenDictionary[key][innerKeys] = innerValue;
+                }
+                else
+                {
+                    flattenDictionary[key][innerKeys] = innerValue;
+                }
+            }
+            else
+            {
+                string key = input[1];
+
+                flattenDictionary[key] = flattenDictionary[key]
+                    .ToDictionary(x => x.Key + x.Value, x => "flattened");
+            }
+        }
+
+        var orderedDictionary = flattenDictionary
+            .OrderByDescending(x => x.Key.Length)
+            .ToDictionary(x => x.Key, x => x.Value);
+
+        byte count = 0;
+        foreach (var print in orderedDictionary)
+        {
+            Console.WriteLine("{0}", print.Key);
+
+            
+            count++;
+        }
+
+
+    }
+}
