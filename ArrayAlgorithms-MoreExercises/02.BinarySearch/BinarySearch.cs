d6b0106c91b7867305b4da53783f4dc5a12bafb4
Peter-Georgiev <peter.g.georgiev@gmail.com>
2017-06-28 23:10:05 +0300

    Added new ArrayAlgorithms-MoreExercises/07. Programming-Fundamentals-Extended-Array-and-List-Algorithms-More-Exercises.docx.

---
+++ ArrayAlgorithms-MoreExercises/02.BinarySearch/BinarySearch.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+
+class BinarySearch
+{
+    static void Main()
+    {
+        int[] firstLine = ReaddLine();
+        int secondLine = int.Parse(Console.ReadLine());
+
+        string message = "No";
+
+        byte countLinear = SearchLinear(firstLine, secondLine, ref message);
+
+        byte countBinary = SearchBinary(firstLine, secondLine);
+
+        PrintResult(message, countLinear, countBinary);
+    }
+
+    static byte SearchBinary(int[] firstLine, int secondLine)
+    {
+        Array.Sort(firstLine);
+
+        byte countBinary = 0;
+        int lowerBound = 0;
+        int upperBound = firstLine.Length - 1;
+
+        while (lowerBound <= upperBound)
+        {
+            if (lowerBound > upperBound)
+            {
+                break;
+            }
+
+            int midPoint = lowerBound + (upperBound - lowerBound) / 2;
+            countBinary++;
+
+            if (firstLine[midPoint] < secondLine)
+            {
+                lowerBound = midPoint + 1;
+            }
+            else if (firstLine[midPoint] > secondLine)
+            {
+                upperBound = midPoint - 1;
+            }
+            else if (firstLine[midPoint] == secondLine)
+            {
+                break;
+            }
+        }
+
+        return countBinary;
+    }
+
+    static byte SearchLinear(int[] firstLine, int secondLine, ref string message)
+    {
+        byte countLinear = 0;
+
+        for (byte i = 0; i < firstLine.Length; i++)
+        {
+            countLinear = i;
+            countLinear++;
+
+            if (firstLine[i] == secondLine)
+            {
+                message = "Yes";
+                break;
+            }
+        }
+
+        return countLinear;
+    }
+
+    static int[] ReaddLine()
+    {
+        int[] readLine = Console.ReadLine()
+            .Split(' ')
+            .Select(n => int.Parse(n))
+            .ToArray();
+
+        return readLine;
+    }
+
+    static void PrintResult(string message, int countLinear, int countBinary)
+    {
+        Console.WriteLine(message);
+        Console.WriteLine("Linear search made {0} iterations", countLinear);
+        Console.WriteLine("Binary search made {0} iterations", countBinary);
+    }
+}
