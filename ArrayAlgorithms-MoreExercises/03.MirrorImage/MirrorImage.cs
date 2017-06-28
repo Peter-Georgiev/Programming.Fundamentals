4464ae52f2fb3a1ccc9fb0898bbbf9bcedd87f83
Peter-Georgiev <peter.g.georgiev@gmail.com>
2017-06-29 00:03:24 +0300

    Added ArrayAlgorithms-MoreExercises/03.MirrorImage/MirrorImage.cs.

---
+++ ArrayAlgorithms-MoreExercises/03.MirrorImage/MirrorImage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MirrorImage
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine()
+            .Split(' ')
+            .ToArray();
+
+        while (true)
+        {
+            string readLine = Console.ReadLine();
+
+            if (readLine.Equals("Print"))
+            {
+                break;
+            }
+
+            int n = int.Parse(readLine);
+
+            input = Reverset(input, 0, n - 1);
+            input = Reverset(input, n + 1, input.Length - 1);
+        }
+
+        Console.WriteLine(string.Join(" ", input));
+    }
+
+    static string[] Reverset(string[] arr, int first, int last)
+    {
+        List<string> temp = new List<string>();
+
+        for (int i = last; i >= first; i--)
+        {
+            temp.Add(arr[i]);
+        }
+
+        for (int i = 0; i < temp.Count; i++)
+        {
+            arr[i + first] = temp[i];
+        }
+
+        return arr;
+    }
+}
