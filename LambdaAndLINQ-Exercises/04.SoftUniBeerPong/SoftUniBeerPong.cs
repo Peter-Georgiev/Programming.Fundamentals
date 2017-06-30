df4b254bb30dd4711a47a30a07acf3c3be2e5ca1
Peter Georgiev <peroxmen@hotmail.com>
2017-07-01 01:23:50 +0300

    1. LambdaAndLINQ-Exercises/04.SoftUniBeerPong/SoftUniBeerPong.cs - WORK. Points :100/100. 2. New project added LambdaAndLINQ-Exercises/05.FlattenDictionary/FlattenDictionary.cs.

--- LambdaAndLINQ-Exercises/04.SoftUniBeerPong/SoftUniBeerPong.cs
+++ LambdaAndLINQ-Exercises/04.SoftUniBeerPong/SoftUniBeerPong.cs
@@ -9,20 +9,103 @@ class SoftUniBeerPong
         Dictionary<string, Dictionary<string, int>> gamesBase =
             new Dictionary<string, Dictionary<string, int>>();
 
-        gamesBase = InsertTeamUser();
+        gamesBase = InsertTeamAndUpToThreeUsers();
 
-        gamesBase = IfOfThree(gamesBase);
+        gamesBase = RemoveLessThanThreeUsers(gamesBase);
 
-        gamesBase = EqualThree(gamesBase);
-
-        var sumDic = TeamPoints(gamesBase);
+        var sumDic = SumTeamsPoints(gamesBase);
 
         PrinTeamUser(gamesBase, sumDic);
     }
-    
-    private static void PrinTeamUser(Dictionary<string, Dictionary<string, int>> gamesBase, Dictionary<string, int> sumDic)
+
+    private static Dictionary<string, Dictionary<string, int>> InsertTeamAndUpToThreeUsers()
     {
-        byte count = 1;
+        var dictionary = new Dictionary<string, Dictionary<string, int>>();
+
+        while (true)
+        {
+            string readLine = Console.ReadLine();
+
+            if (readLine.Equals("stop the game"))
+            {
+                break;
+            }
+            else
+            {
+                string[] input = readLine
+                    .Split(new[] { ' ', '|' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string player = input[0];
+                string team = input[1];
+                int pointsMade = int.Parse(input[2]);
+                
+                if (!dictionary.ContainsKey(team))
+                {
+                    dictionary.Add(team, new Dictionary<string, int>());
+                    dictionary[team][player] = pointsMade;
+                }
+
+                foreach (var kvpTeam in dictionary)
+                {
+                    string teamKey = kvpTeam.Key;
+                    var players = kvpTeam.Value;
+
+                    if (teamKey == team)
+                    {
+                        if (players.Count() < 3)
+                        {
+                            dictionary[team][player] = pointsMade;
+                            break;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return dictionary;
+    }
+
+    private static Dictionary<string, Dictionary<string, int>> RemoveLessThanThreeUsers(Dictionary<string, Dictionary<string, int>> gamesBase)
+    {
+        var dictionary = gamesBase
+            .Where(x => x.Value.Keys.Count.Equals(3))
+            .ToDictionary(x => x.Key, x => x.Value);
+
+        return dictionary;
+    }
+   
+    private static Dictionary<string, long> SumTeamsPoints(
+        Dictionary<string, Dictionary<string, int>> gamesBase)
+    {
+        var dictionary = new Dictionary<string, long>();
+
+        foreach (var kvpBase in gamesBase)
+        {
+            string team = kvpBase.Key;
+            long sum = kvpBase.Value.Values.Sum();
+
+            foreach (var kvpPlayer in kvpBase.Value)                
+            {
+                if (!dictionary.ContainsKey(team))
+                {
+                    dictionary.Add(team, sum);
+                }
+            }            
+        }
+
+        return dictionary
+            .OrderByDescending(x => x.Value)
+            .ToDictionary(x => x.Key, x => x.Value);
+    }
+
+    private static void PrinTeamUser(Dictionary<string, Dictionary<string, int>> gamesBase, Dictionary<string, long> sumDic)
+    {
+        int count = 1;
 
         foreach (var kvpSum in sumDic)
         {
@@ -44,111 +127,8 @@ class SoftUniBeerPong
                     }
                 }
             }
+
             count++;
         }
-    }
-
-    private static Dictionary<string, int> TeamPoints(
-        Dictionary<string, Dictionary<string, int>> gamesBase)
-    {
-        var dictionary = 
-            new Dictionary<string, int>();
-
-        foreach (var kvpBase in gamesBase)
-        {
-            string team = kvpBase.Key;
-            int sum = kvpBase.Value.Values.Sum();
-
-            foreach (var kvpPlayer in kvpBase.Value)                
-            {
-                if (!dictionary.ContainsKey(team))
-                {
-                    dictionary.Add(team, sum);
-                }
-            }
-            
-        }
-
-        return dictionary
-            .OrderByDescending(x => x.Value)
-            .ToDictionary(x => x.Key, x => x.Value);
-    }    
-      
-    private static Dictionary<string, Dictionary<string, int>> InsertTeamUser()
-    {
-        var dictionary =
-            new Dictionary<string, Dictionary<string, int>>();
-
-        while (true)
-        {
-            string readLine = Console.ReadLine();
-
-            if (readLine.Equals("stop the game"))
-            {
-                break;
-            }
-            else
-            {
-                string[] input = readLine
-                    .Split(new[] { ' ', '|' }, StringSplitOptions.RemoveEmptyEntries)
-                    .ToArray();
-
-                string player = input[0];
-                string team = input[1];
-                int pointsMade = int.Parse(input[2]);
-
-                if (!dictionary.ContainsKey(team))
-                {
-                    dictionary.Add(team, new Dictionary<string, int>());
-                    dictionary[team][player] = pointsMade;
-                }
-
-                var t = dictionary.Values
-                 .SelectMany(list => list)
-                 .Distinct()
-                 .Count();
-
-            }
-        }
-        return dictionary;
-    }
-
-    private static Dictionary<string, Dictionary<string, int>> IfOfThree(Dictionary<string, Dictionary<string, int>> gamesBase)
-    {
-        var dictionary = gamesBase
-            .Where(x => x.Value.Keys.Count >= 3)
-            .ToDictionary(x => x.Key, x => x.Value);
-        return dictionary;
-    }
-
-    private static Dictionary<string, Dictionary<string, int>> EqualThree(Dictionary<string, Dictionary<string, int>> gamesBase)
-    {
-        var dictionary = new Dictionary<string, Dictionary<string, int>>();
-
-        foreach (var kvp in gamesBase)
-        {
-            string team = kvp.Key;
-            Dictionary<string, int> playerdic = kvp.Value;
-
-            dictionary.Add(team, new Dictionary<string, int>());
-
-            byte count = 0;
-
-            foreach (var kvpplayer in playerdic)
-            {
-                string player = kvpplayer.Key;
-                int pointmode = kvpplayer.Value;
-                if (count < 3)
-                {
-                    dictionary[team][player] = pointmode;
-                }
-                else
-                {
-                    break;
-                }
-                count++;
-            }
-        }
-        return dictionary;
     }
 }
