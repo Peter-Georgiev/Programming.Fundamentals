dd45f4e6285fead310b3958f20c95bab6e8b4923
Peter Georgiev <peroxmen@hotmail.com>
2017-07-03 22:26:22 +0300

    LambdaAndLINQ-MoreExercises/01.LambadaExpressions/LambadaExpressions.cs -WORKS. Points: 100/100. LambdaAndLINQ-MoreExercises/06.OrderedBankingSystem/OrderedBankingSystem.cs - WORKS. Points: 100/100.

--- LambdaAndLINQ-MoreExercises/01.LambadaExpressions/LambadaExpressions.cs
+++ LambdaAndLINQ-MoreExercises/01.LambadaExpressions/LambadaExpressions.cs
@@ -6,7 +6,62 @@ class LambadaExpressions
 {
     static void Main()
     {
+        var dictionary = new Dictionary<string, string>();
 
+        while (true)
+        {
+            string readLine = Console.ReadLine();
 
+            if (readLine.Equals("lambada"))
+            {
+                PrintResult(dictionary);
+                break;
+            }
+            else if (readLine.Equals("dance"))
+            {
+                GetDance(dictionary);
+            }
+            else
+            {
+                InsertSelector(dictionary, readLine);
+            }
+        }
+    }
+
+    private static void GetDance(Dictionary<string, string> dictionary)
+    {
+        var tempDic = dictionary
+                            .ToDictionary(x => x.Key, x => x.Key + "." + x.Value);
+
+        foreach (var kvpTempDic in tempDic)
+        {
+            dictionary[kvpTempDic.Key] = kvpTempDic.Value;
+        }
+    }
+
+    private static void PrintResult(Dictionary<string, string> dictionary)
+    {
+        foreach (var kvp in dictionary)
+        {
+            Console.WriteLine("{0} => {1}", kvp.Key, kvp.Value);
+        }
+    }
+
+    private static void InsertSelector(Dictionary<string, string> dictionary, string readLine)
+    {
+        string[] tokens = readLine
+                            .Split(" =>.".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                            .ToArray();
+
+        string selector = tokens[0];
+        string selectorObject = tokens[1];
+        string property = tokens[2];
+
+        if (!dictionary.ContainsKey(selector))
+        {
+            dictionary.Add(selector, selectorObject + "." + property);
+        } 
+
+        dictionary[selector] = selectorObject + "." + property;
     }
 }
---
+++ LambdaAndLINQ-MoreExercises/06.OrderedBankingSystem/OrderedBankingSystem.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class OrderedBankingSystem
+{
+    static void Main()
+    {
+        var baseBank = new Dictionary<string, Dictionary<string, decimal>>();
+
+        while (true)
+        {
+            string readLine = Console.ReadLine();
+
+            if (readLine.Equals("end"))
+            {
+                break;
+            }
+            else
+            {
+                InsertBaseBank(baseBank, readLine);
+            }
+        }
+
+        PrintResult(baseBank);        
+    }
+
+    private static void PrintResult(Dictionary<string, Dictionary<string, decimal>> baseBank)
+    {
+        baseBank
+            .OrderByDescending(bank => bank.Value.Sum(account => account.Value))
+            .ThenByDescending(bank => bank.Value.Max(account => account.Value))
+            .ToList()
+            .ForEach(bank => bank.Value.OrderByDescending(account => account.Value)
+            .ToList()
+            .ForEach(account => Console.WriteLine($"{account.Key} -> {account.Value} ({bank.Key})")));
+    }
+
+    private static void InsertBaseBank(Dictionary<string, Dictionary<string, decimal>> baseBank, string readLine)
+    {
+        string[] tokens = readLine
+            .Split(new char[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries)
+            .ToArray();
+        string bank = tokens[0];
+        string account = tokens[1];
+        decimal balance = decimal.Parse(tokens[2]);
+
+        if (!baseBank.ContainsKey(bank))
+        {
+            baseBank.Add(bank, new Dictionary<string, decimal>());            
+        }
+
+        if (!baseBank[bank].ContainsKey(account))
+        {
+            baseBank[bank].Add(account, 0);
+        }
+
+        baseBank[bank][account] += balance;
+    }
+}
