9459c1b32d8ceecc0417c91b05e90d794780f8bc
Peter-Georgiev <peter.g.georgiev@gmail.com>
2017-07-12 14:14:24 +0300

    Added FilesAndExceptions-Exercises.sln. This exercise does NOT have a Judge Contest. You will have to test every problem locally. Added FilesAndExceptions-Exercises/01.FilterExtensions/FilterExtensions.cs - WORKS.

---
+++ FilesAndExceptions-Exercises/01.FilterExtensions/FilterExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class FilterExtensions
+{
+    static void Main()
+    {
+        string inputFolder = @"..\..\..\Exercises-Resource\01. Filter-Extensions\input";
+
+        string[] allFilesDir = Directory.GetFiles(inputFolder);
+
+        string readLine = Console.ReadLine().ToLower();
+
+        for (int i = 0; i < allFilesDir.Length; i++)
+        {
+            var fileName = new FileInfo(allFilesDir[i]).Name;
+            string[] extensionFile = fileName.Split('.').ToArray();
+
+            if (readLine == extensionFile[extensionFile.Length - 1])
+            {
+                Console.WriteLine(fileName);
+            }
+        }
+    }
+}
