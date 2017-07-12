f6db75fc565b259df84d40714109cf98f1b481b7
Peter-Georgiev <peter.g.georgiev@gmail.com>
2017-07-12 17:23:24 +0300

    Added FilesAndExceptions-Exercises.sln.

---
+++ FilesAndExceptions-Exercises/02.HTMLcontents/HTMLcontents.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+
+class HTMLcontents
+{
+    static void Main()
+    {
+        string[] contents = new string[]
+        {
+            "<!DOCTYPE html>" + Environment.NewLine,
+             "<html>" + Environment.NewLine,
+             "<head>" + Environment.NewLine,
+             "</head>" + Environment.NewLine,
+             "<body>" + Environment.NewLine,
+             "</body>" + Environment.NewLine,
+             "</html>" + Environment.NewLine
+        };
+
+        var input = new Dictionary<string, List<string>>();
+
+        ReadInputLine(input);
+
+        WriteIndexFile(input, contents);
+    }
+
+    private static void WriteIndexFile(Dictionary<string, List<string>> input, string[] contents)
+    {
+        string indexFile = "index.html";
+
+        List<string> body = new List<string>();
+        string[] head = new string[3];
+        head[0] = contents[2];
+        head[2] = contents[3];
+        
+        foreach (KeyValuePair<string, List<string>> kvp in input)
+        {
+            string tag = kvp.Key;
+
+            if (tag.Equals("title"))
+            {
+                string[] content = kvp.Value.ToArray();
+
+                head[1] = $"\t<title>{content[content.Length - 1]}</title>" +
+                    Environment.NewLine;
+            }
+            else
+            {
+                foreach (var content in kvp.Value)
+                {
+                    body.Add($"\t<{tag}>{content}</{tag}>" + Environment.NewLine);
+                }
+            }
+        }
+
+        File.Delete(indexFile);
+
+        File.AppendAllText(indexFile, contents[0]);
+        File.AppendAllText(indexFile, contents[1]);
+        
+        foreach (var item in head)
+        {
+            File.AppendAllText(indexFile, item);
+        }
+
+        foreach (var item in body)
+        {
+            File.AppendAllText(indexFile, item);
+        }
+
+        File.AppendAllText(indexFile, contents[6]);
+    }
+
+    static void ReadInputLine(Dictionary<string, List<string>> input)
+    {
+
+        while (true)
+        {
+            string[] tokens = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+
+            string tag = tokens[0];
+
+            if (tag.Equals("exit"))
+            {
+                break;
+            }
+
+            string content = tokens[1];
+
+            if (!input.ContainsKey(tag))
+            {
+                input.Add(tag, new List<string>());
+            }
+
+            input[tag].Add(content);            
+        }
+    }
+}
---
+++ FilesAndExceptions-Exercises/03.UserDatabase/UserDatabase.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+class UserDatabase
+{
+    static void Main()
+    {
+        var database = new Dictionary<string, string>();
+
+        ReadLineandInsertDatabase(database);
+    }
+
+    private static void ReadLineandInsertDatabase(Dictionary<string, string> database)
+    {
+        while (true)
+        {
+            string[] tokens = ReadLine();
+            string command = tokens[0];
+
+            if (command.Equals("exit"))
+            {
+                break;
+            }
+
+            if (command.Equals("register"))
+            {
+                string userName = tokens[1];
+
+                if (!database.ContainsKey(userName))
+                {
+                    database.Add(userName, String.Empty);
+
+                    bool isPassword = true;
+                    while (isPassword)
+                    {
+                        if (tokens[2] == tokens[3])
+                        {
+                            database[userName] = tokens[2];
+                            isPassword = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("The two passwords must match.");
+                        }
+
+                        tokens = ReadLine();
+                        command = tokens[0];
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("The given username already exists.");
+                }
+            }
+
+            bool login = command.Equals("login");
+
+            while (login)
+            {
+
+
+
+
+
+
+
+
+                if (command.Equals("logout"))
+                {
+                    login = false;
+                    break;
+                }
+
+                tokens = ReadLine();
+                command = tokens[0];
+            }
+        }
+    }
+
+    static string[] ReadLine()
+    {
+        string[] tokens = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+        return tokens;
+    }
+}
