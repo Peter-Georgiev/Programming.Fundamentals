fca467fe0ad3bb14ace75900661173a2acdc9dfd
Peter-Georgiev <peter.g.georgiev@gmail.com>
2017-07-21 16:46:22 +0300

    Added RegularExpressions(RegEx)-Lab.sln.

---
+++ RegularExpressions(RegEx)-Lab/01.MatchFullName/MatchFullName.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class MatchFullName
+{
+    static void Main()
+    {
+        string rexex = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+        string names = Console.ReadLine();
+
+        MatchCollection matchedNames = Regex.Matches(names, rexex);
+
+        foreach (Match name in matchedNames)
+        {
+            Console.Write(name.Value + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
---
+++ RegularExpressions(RegEx)-Lab/02.MatchPhoneNumber/MatchPhoneNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class MatchPhoneNumber
+{
+    static void Main()
+    {
+        //string regex = @"";
+        string text = "12.02.2017";
+
+        string pattern = @"([0-9]{2})";
+
+        //Regex regex = new Regex(pattern);
+
+        bool isr = Regex.IsMatch(text, pattern);
+        Console.WriteLine();
+    }
+}
