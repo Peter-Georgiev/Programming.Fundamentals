4bd9215437e2c38dd094385f9e9255bd631ac26f
Peter Georgiev <peroxmen@hotmail.com>
2017-07-27 23:53:44 +0300

    Added ExamPreparation II-T.S.Exam/02.Ladybugs/Ladybugs.cs, ExamPreparation II-T.S.Exam/03.NetherRealms/NetherRealms.cs.

--- ExamPreparation II-T.S.Exam/02.Ladybugs/Ladybugs.cs
+++ ExamPreparation II-T.S.Exam/02.Ladybugs/Ladybugs.cs
@@ -1,15 +1,100 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 class Ladybugs
 {
     static void Main()
     {
+        int[] sizeFilds = new int[int.Parse(Console.ReadLine())];
+        int[] indexes = Regex.Split(Console.ReadLine(), @"\s+")
+            .Select(x => x.Trim())
+            .Select(int.Parse)
+            .Where(x => x >= 0 && x <= sizeFilds.Length - 1)
+            .ToArray();
 
+        foreach (var i in indexes)
+        {
+            sizeFilds[i] = 1;
+        }
 
+        while (true)
+        {
+            string command = Console.ReadLine();
+            if (command.Equals("end"))
+            {
+                break;
+            }
 
+            string[] tokens = Regex.Split(command, @"\s+")
+                .Select(x => x.Trim())
+                .ToArray();
+
+            int ladybugIndex = int.Parse(tokens[0]);
+            string direction = tokens[1];
+            int flyLength = int.Parse(tokens[2]);
+
+            switch (direction)
+            {
+                case "right":
+                    GetCommandRight(sizeFilds, ladybugIndex, flyLength);
+                    break;
+                case "left":
+                    GetCommandLeft(sizeFilds, ladybugIndex, flyLength);
+                    break;
+            }
+        }
+
+        Console.WriteLine(String.Join(" ", sizeFilds));
+    }
+
+    static void GetCommandLeft(int[] sizeFilds, int ladybugIndex, int flyLength)
+    {
+        int flyCount = flyLength;
+        int i = sizeFilds.Length - 1;
+        while (i >= 0)
+        {
+            if (i == ladybugIndex)
+            {
+                sizeFilds[i] = 0;
+                break;
+            }
+
+            if (i == Math.Abs(ladybugIndex - flyCount) && sizeFilds[i] != 1)
+            {
+                sizeFilds[i] = 1;
+            }
+            else if (i == Math.Abs(ladybugIndex - flyCount))
+            {
+                flyCount = Math.Abs(i - flyLength);
+            }
+
+            i--;
+        }
+    }
+
+    static void GetCommandRight(int[] sizeFilds, int ladybugIndex, int flyLength)
+    {
+        int flyCount = flyLength;
+        int i = 0;
+        while (i <= sizeFilds.Length - 1)
+        {
+            if (i == ladybugIndex)
+            {
+                sizeFilds[i] = 0;                
+            }
+
+            if (i == ladybugIndex + flyCount && sizeFilds[i] != 1)
+            {
+                sizeFilds[i] = 1;
+                break;
+            }
+            else if (i == ladybugIndex + flyCount)
+            {
+                flyCount = i + flyLength;
+            }
+
+            i++;
+        }    
     }
 }
---
+++ ExamPreparation II-T.S.Exam/03.NetherRealms/NetherRealms.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Demon
+{
+    public string Name { get; set; }
+    public int Health { get; set; }
+    public double Damage { get; set; }
+}
+
+class NetherRealms
+{
+    static void Main()
+    {
+        List<Demon> demons = new List<Demon>();
+
+        string[] tokens = Regex.Split(Console.ReadLine(), @",\s+")
+            .Select(x => x.Trim())
+            .ToArray();
+
+        for (int i = 0; i < tokens.Length; i++)
+        {
+            MatchCollection regex = Regex.Matches(tokens[i], @"(?<number>-\d.\d|\d.\d|\d)|(?<characters>[^*+-\/])|(?<arithmetic>.)");
+
+            int healt = 0;
+            double damage = 0.00d;
+            List<char> arithmetic = new List<char>();
+
+            foreach (Match item in regex)
+            {
+                if (item.Groups["characters"].Value != String.Empty)
+                {
+                    healt += char.Parse(item.Groups["characters"].Value);
+                }
+
+                if (double.TryParse(item.Groups["number"].Value, out double d))
+                {
+                    damage += d;
+                }
+                else if (int.TryParse(item.Groups["number"].Value, out int n))
+                {
+                    damage += n;
+                }
+
+
+                if (item.Groups["arithmetic"].Value != String.Empty)
+                {
+                    char[] ch = new char[] { '*', '+', '-', '/' };
+                    if (ch.Contains(char.Parse(item.Groups["arithmetic"].Value)))
+                    {
+                        arithmetic.Add(char.Parse(item.Groups["arithmetic"].Value));
+                    }
+                }
+            }
+
+            foreach (var calc in arithmetic)
+            {
+                switch (calc)
+                {
+                    case '*':
+                        damage *= 2.00;
+                        break;
+                    case '/':
+                        damage /= 2.00;
+                        break;
+                }
+            }
+
+            Demon newDemon = new Demon
+            {
+                Name = tokens[i],
+                Health = healt,
+                Damage = damage
+            };
+
+            demons.Add(newDemon);
+        }
+
+        PrintResult(demons);
+    }
+
+    private static void PrintResult(List<Demon> demons)
+    {
+        List<Demon> result = demons
+                    .OrderBy(x => x.Name)
+                    .ToList();
+        foreach (var d in result)
+        {
+            Console.WriteLine($"{d.Name} - {d.Health} health, {d.Damage:F2} damage");
+        }
+    }
+}
