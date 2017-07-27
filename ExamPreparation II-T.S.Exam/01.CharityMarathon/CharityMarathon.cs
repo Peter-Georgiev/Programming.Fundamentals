8938e8abc92d8691707226ad2ecfe35aade6c6c7
Peter-Georgiev <peter.g.georgiev@gmail.com>
2017-07-27 15:27:40 +0300

    Added ExamPreparation II-T.S.Exam.sln.

--- ExamPreparation II-T.S.Exam/01.CharityMarathon/CharityMarathon.cs
+++ ExamPreparation II-T.S.Exam/01.CharityMarathon/CharityMarathon.cs
@@ -1,15 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 class CharityMarathon
 {
     static void Main()
     {
+        long days = int.Parse(Console.ReadLine()); //range [0 … 365]
+        long participate = long.Parse(Console.ReadLine()); //range [0 … 2,147,483,647]
+        int laps = int.Parse(Console.ReadLine()); //range [0 … 100]
+        long lengthTrack = long.Parse(Console.ReadLine()); //[0 … 2,147,483,647]
+        long capacityTrack = int.Parse(Console.ReadLine()); //range [0 … 1000]
+        decimal moneyKm = decimal.Parse(Console.ReadLine()); //floating point number
 
+        long maxRunners = capacityTrack * days;
 
+        long minRunners = Math.Min(maxRunners, participate);
+
+        long totalKilometers = (minRunners * laps * lengthTrack) / 1000;
+
+        decimal moneyRaised = totalKilometers * moneyKm;
+
+        Console.WriteLine($"Money raised: {moneyRaised:F2}");
     }
 }
