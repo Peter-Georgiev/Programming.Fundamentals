10bed00938461da9cbd35b5ee639f7363fb81f9f
Peter Georgiev <peroxmen@hotmail.com>
2017-07-30 22:02:49 +0300

    Added ExamPreparation III-T.S.Exam.sln and ExamPreparation III-T.S.Exam/01. Softuni Coffee Orders_Условие.docx. ExamPreparation III-T.S.Exam/01.SoftuniCoffeeOrders/SoftuniCoffeeOrders.cs - WORKS. Points: 100/100. ExamPreparation III-T.S.Exam/02.CommandInterpreter/CommandInterpreter.cs - points: 5/100.

---
+++ ExamPreparation III-T.S.Exam/01.SoftuniCoffeeOrders/SoftuniCoffeeOrders.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+class SoftuniCoffeeOrders
+{
+    static void Main()
+    {
+        decimal totalPrice = 0.00m;
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            CultureInfo provider = CultureInfo.InvariantCulture;
+
+            decimal pricePerCapsule = decimal.Parse(Console.ReadLine());
+            string[] date = Console.ReadLine().Split('/');
+            // DateTime date = DateTime.ParseExact(Console.ReadLine(), "d/M/yyyy", provider);
+            int capsulesCount = int.Parse(Console.ReadLine());
+
+            int daysInMonth = DateTime.DaysInMonth(int.Parse(date[2]), int.Parse(date[1]));
+
+            decimal price = (((decimal)daysInMonth * (decimal)capsulesCount) * pricePerCapsule);
+            totalPrice += price;
+            Console.WriteLine($"The price for the coffee is: ${price:F2}");
+        }
+
+        Console.WriteLine($"Total: ${totalPrice:F2}");
+    }
+}
---
+++ ExamPreparation III-T.S.Exam/02.CommandInterpreter/CommandInterpreter.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class CommandInterpreter
+{
+    static void Main()
+    {
+        List<string> input = Console.ReadLine()
+            .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .ToList();
+        string[] result = new string[input.Count];
+        while (true)
+        {
+            string command = Console.ReadLine();
+            if (command.Equals("end"))
+            {
+                break;
+            }
+
+            Match regex = Regex.Match(command, @"(?<1>reverse(?:\s+from\s+-?\d+\s+count\s+-?\d+))|(?<2>sort(?:\s+from\s+-?\d+\s+count\s+-?\d+))|(?<3>rollLeft(?:\s+-?\d+\s+times))|(?<4>rollRight(?:\s+-?\d+\s+times))");
+
+            if (!regex.Success)
+            {
+                continue;
+            }
+
+            string[] tokens = new string[0];
+            for (int i = 0; i < regex.Groups.Count; i++)
+            {
+                if (regex.Groups[$"{i}"].Length > 0)
+                {
+                    tokens = tokens = regex.Groups[$"{i}"].Value.ToLower()
+                        .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                        .ToArray();
+                }
+            }
+
+            if (tokens[0] == "reverse")
+            {
+                int fromStart = int.Parse(tokens[2]);
+                int count = int.Parse(tokens[4]);
+
+                if ((fromStart < 0 || fromStart >= input.Count) ||
+                    (fromStart + count < 0 || fromStart + count >= input.Count))
+                {
+                    Console.WriteLine("Invalid input parameters.");
+                    continue;
+                }
+
+                var tempRevers = input
+                    .Skip(fromStart)
+                    .Take(count)
+                    .Reverse()
+                    .ToList();
+
+                input.RemoveRange(fromStart, count);
+                input.InsertRange(fromStart, tempRevers);
+
+                for (int i = 0; i < input.Count; i++)
+                {
+                    result[i] = input[i];
+                }
+            }
+            else if (tokens[0] == "sort")
+            {
+                int fromStart = int.Parse(tokens[2]);
+                int count = int.Parse(tokens[4]);
+
+                if ((fromStart < 0 || fromStart >= input.Count) ||
+                    (fromStart + count < 0 || fromStart + count >= input.Count))
+                {
+                    Console.WriteLine("Invalid input parameters.");
+                    continue;
+                }
+
+                var tempSorted = input
+                   .Skip(fromStart)
+                   .Take(count)
+                   .OrderBy(x => x)
+                   .ToList();
+
+                input.RemoveRange(fromStart, count);
+                input.InsertRange(fromStart, tempSorted);
+                for (int i = 0; i < input.Count; i++)
+                {
+                    result[i] = input[i];
+                }
+            }
+            else if (tokens[0] == "rollleft")
+            {
+                int count = int.Parse(tokens[1]);
+
+                for (int oldIndex = 0; oldIndex < input.Count; oldIndex++)
+                {
+                    int newIndex = oldIndex + (-count);
+                    newIndex = newIndex % input.Count;
+
+                    if (newIndex < 0)
+                    {
+                        newIndex += input.Count;
+                    }
+
+                    result[newIndex] = input[oldIndex];
+                }
+            }
+            else if (tokens[0] == "rollright")
+            {
+                int count = int.Parse(tokens[1]);
+                
+                for (int oldIndex = 0; oldIndex < input.Count; oldIndex++)
+                {
+                    int newIndex = oldIndex + count;
+                    newIndex = newIndex % input.Count;
+
+                    if (newIndex < 0)
+                    {
+                        newIndex += input.Count;
+                    }
+
+                    result[newIndex] = input[oldIndex];
+                }
+            }
+        }
+
+        Console.WriteLine("[" + string.Join(", ", result) + "]");
+    }
+}
