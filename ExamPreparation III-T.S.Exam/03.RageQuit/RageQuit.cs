8c3098523d43ddd6c515bd1c3c32c7786637cbe7
Peter-Georgiev <peter.g.georgiev@gmail.com>
2017-08-01 13:33:28 +0300

    ExamPreparation III-T.S.Exam/03.RageQuit/RageQuit.cs - WORKS. Points: 100/100.

---
+++ ExamPreparation III-T.S.Exam/03.RageQuit/RageQuit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class RageQuit
+{
+    static void Main()
+    {
+        string readLine = Console.ReadLine().ToUpper();
+
+        MatchCollection regex =
+            Regex.Matches(readLine, @"(?<word>.+?)((?<number>\d\d?))");
+
+        StringBuilder result = new StringBuilder();
+
+        foreach (Match l in regex)
+        {
+            for (int i = 0; i < int.Parse(l.Groups["number"].Value); i++)
+            {
+                result.Append($"{l.Groups["word"].Value}");
+            }
+        }
+
+        int uniqueSymbol = result
+            .ToString()
+            .Distinct()
+            .Count();
+
+        Console.WriteLine($"Unique symbols used: {uniqueSymbol}");
+        Console.WriteLine(result);
+    }
+}
