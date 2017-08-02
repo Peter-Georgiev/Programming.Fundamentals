9c8df389e289db99834c16ef7ca21f92c606ba12
Peter-Georgiev <peter.g.georgiev@gmail.com>
2017-08-02 14:29:31 +0300

    StringsTextProcessing-MoreExer/04.DeserializeString/DeserializeString.cs - WORKS. Points: 100/100. StringsTextProcessing-MoreExer/05.StringCommander/StringCommander.cs - WORKS. Points: 100/100.

--- StringsTextProcessing-MoreExer/04.DeserializeString/DeserializeString.cs
+++ StringsTextProcessing-MoreExer/04.DeserializeString/DeserializeString.cs
@@ -1,13 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 class DeserializeString
 {
     static void Main()
     {
+        SortedDictionary<int, char> result = new SortedDictionary<int, char>();
+        while (true)
+        {
+            string command = Console.ReadLine();
+            if (command.Equals("end"))
+            {
+                break;
+            }
 
+            string[] tokens = command
+                .Split(new[] { ':' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            int[] indices = tokens[1]
+                .Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            char ch = Convert.ToChar(tokens[0]);
+                        
+            for (int i = 0; i < indices.Length; i++)
+            {
+                if (!result.ContainsKey(indices[i]))
+                {
+                    result.Add(indices[i], ch);
+                }
+            }            
+        }
+
+        result
+            .Select(x => x.Value)
+            .ToList()
+            .ForEach(x => Console.Write(x));
+        Console.WriteLine();
     }
 }
--- StringsTextProcessing-MoreExer/05.StringCommander/StringCommander.cs
+++ StringsTextProcessing-MoreExer/05.StringCommander/StringCommander.cs
@@ -1,13 +1,98 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 class StringCommander
 {
     static void Main()
     {
+        string input = Console.ReadLine();
 
+        while (true)
+        {
+            string command = Console.ReadLine();
+            if (command.Equals("end"))
+            {
+                break;
+            }
+
+            string[] tokens = command
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            command = tokens[0];
+
+            switch (command)
+            {
+                case "Left":
+                    LeftCountTimes(tokens, ref input);
+                    break;
+                case "Right":
+                    RightCountTimes(tokens, ref input);
+                    break;
+                case "Insert":
+                    InsertStringIndex(tokens, ref input);
+                    break;
+                case "Delete":
+                    DeleteStartEndIndex(tokens, ref input);
+                    break;
+            }
+        }
+
+        Console.WriteLine(input);
+    }
+
+    private static void RightCountTimes(string[] tokens, ref string input)
+    {
+        int count = int.Parse(tokens[1]) % input.Length;
+        char[] temp = input.ToCharArray(); ;
+
+        for (int times = 0; times < count; times++)
+        {
+            char lastElement = temp[temp.Length - 1];
+
+            for (int i = temp.Length - 1; i > 0; i--)
+            {
+                temp[i] = temp[i - 1];
+            }
+
+            temp[0] = lastElement;
+        }
+
+        input = string.Join("", temp);
+    }
+
+    private static void LeftCountTimes(string[] tokens, ref string input)
+    {
+        int count = int.Parse(tokens[1]) % input.Length;
+        char[] temp = input.ToCharArray();
+
+        for (int times = 0; times < count; times++)
+        {
+            char firstelement = temp[0];
+
+            for (int i = 0; i < temp.Length - 1; i++)
+            {
+                temp[i] = temp[i + 1];
+            }
+
+            temp[temp.Length - 1] = firstelement;
+        }
+
+        input = string.Join("", temp);
+    }
+
+    private static void InsertStringIndex(string[] tokens, ref string input)
+    {
+        int index = int.Parse(tokens[1]);
+        string str = tokens[2];
+
+        input = input.Insert(index, str);
+    }
+
+    private static void DeleteStartEndIndex(string[] tokens, ref string input)
+    {
+        int startIndex = int.Parse(tokens[1]);
+        int endIndex = int.Parse(tokens[2]);
+
+        input = input.Remove(startIndex, endIndex - startIndex + 1);
     }
 }
