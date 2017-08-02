51327d4701c53642428e6d607200139089bfd835
Peter-Georgiev <peter.g.georgiev@gmail.com>
2017-08-02 16:22:23 +0300

    Added ExamPreparation IV-T.S.Exam/03.FootballLeague/FootballLeague.cs

---
+++ ExamPreparation IV-T.S.Exam/03.FootballLeague/FootballLeague.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class FootballLeague
+{
+    static void Main()
+    {
+        string key = Console.ReadLine();
+
+        while (true)
+        {
+            string command = Console.ReadLine();
+            if (command.Equals("final"))
+            {
+                break;
+            }
+
+            string pattern = @"(?<name>\w+).*?";
+            pattern =  pattern.Insert(8, key);
+            pattern = pattern.Insert(11 + key.Length, key);
+
+            Console.WriteLine(pattern);
+
+
+        }
+    }
+}
