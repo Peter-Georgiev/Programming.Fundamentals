d2c83443bf6f2846f9ef520925b09bfb823e791e
Peter Georgiev <peroxmen@hotmail.com>
2017-08-05 12:34:42 +0300

    ExamPreparation IV-T.S.Exam/04.CubicMessages/CubicMessages.cs - WORKS. Points: 100/100.

--- ExamPreparation IV-T.S.Exam/04.CubicMessages/CubicMessages.cs
+++ ExamPreparation IV-T.S.Exam/04.CubicMessages/CubicMessages.cs
@@ -1,13 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 class CubicMessages
 {
     static void Main()
     {
+        while (true)
+        {
+            string readLine = Console.ReadLine();
+            if (readLine.Equals("Over!"))
+            {
+                break;
+            }
+            int lengthMessage = int.Parse(Console.ReadLine());
 
+            Regex regex = 
+                new Regex($@"(?<left>^\d+)(?<word>[A-Za-z]{{{lengthMessage}}})(?<right>[^A-Za-z]*$)");
+
+            Match input = regex.Match(readLine);
+            string message = input.Groups["word"].Value;             
+
+            if (!input.Success)
+            {
+                continue;
+            }
+
+            string leftStr = input.Groups["left"].Value;
+            string rightStr = input.Groups["right"].Value;
+            
+            int[] indexes = string.Concat(leftStr, rightStr)
+                .Where(Char.IsDigit)
+                .Select(x => x - '0')                
+                .ToArray();
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (var index in indexes)
+            {                
+                if (index < 0 || index >= message.Length)
+                {
+                    result.Append(' ');
+                }
+                else
+                {
+                    result.Append(message[index]);
+                }
+            }
+
+            Console.WriteLine($"{message} == {result}");
+        }
     }
 }
