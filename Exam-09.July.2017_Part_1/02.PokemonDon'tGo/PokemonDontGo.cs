97482807191b9178c455c76c23b900b96f066aca
Peter-Georgiev <peter.g.georgiev@gmail.com>
2017-08-11 17:23:11 +0300

    Added Exam-09.July.2017_Part_1.sln.

---
+++ Exam-09.July.2017_Part_1/02.PokemonDon'tGo/PokemonDontGo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class PokemonDontGo
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int m = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+
+        int originalN = n;
+
+        int count = 0;
+        while (n > m)
+        {
+            n -= m;
+            count++;
+        }
+        
+        decimal percentM = ((long)m * (long)originalN) / 100m;
+        decimal percentN = ((long)originalN * (long)m) / 100m;
+
+        if (n < m)
+        {
+            Console.WriteLine(n);
+            Console.WriteLine(count);
+        }
+        else if (n == m)
+        {
+            if (n >= y && n != 0 && y != 0)
+            {
+                int division = n / y;
+                Console.WriteLine(division);
+            }
+            else
+            {
+                while (n >= m)
+                {
+                    n -= m;
+                }
+                Console.WriteLine(n);
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
