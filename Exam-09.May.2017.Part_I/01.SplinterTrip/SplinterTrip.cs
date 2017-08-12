6afcbe867e7726798f504e2ef20cb4db45c580aa
Peter Georgiev <peroxmen@hotmail.com>
2017-08-12 14:25:16 +0300

    Exam-09.May.2017.Part_I/01.SplinterTrip/SplinterTrip.cs - WORKS. Points: 100/100. Exam-09.May.2017.Part_I/02.SpyGram/SpyGram.cs - WORKS. Points: 100/100.

---
+++ Exam-09.May.2017.Part_I/01.SplinterTrip/SplinterTrip.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class SplinterTrip
+{
+    static void Main()
+    {
+        decimal tripDistanceMiles = decimal.Parse(Console.ReadLine());
+        decimal fuelTankLiters = decimal.Parse(Console.ReadLine());
+        decimal milesInHeavyWinds = decimal.Parse(Console.ReadLine());
+
+        decimal milesNonHeavyWinds = (tripDistanceMiles - milesInHeavyWinds) * 25m;
+        decimal litersHeavyWinds = milesInHeavyWinds * (25 * 1.5m);
+        decimal totalLitersFuelConsumption = 
+            (((milesNonHeavyWinds + litersHeavyWinds) * 5) / 100m) +
+            (milesNonHeavyWinds + litersHeavyWinds);
+        decimal remainingFuel = fuelTankLiters - totalLitersFuelConsumption;
+
+        Console.WriteLine($"Fuel needed: {totalLitersFuelConsumption:F2}L");
+
+        if (remainingFuel >= 0)
+        {
+            Console.WriteLine($"Enough with {remainingFuel:F2}L to spare!");
+        }
+        else
+        {
+            Console.WriteLine($"We need {Math.Abs(remainingFuel):F2}L more fuel.");
+        }
+    }
+} 
---
+++ Exam-09.May.2017.Part_I/02.SpyGram/SpyGram.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Text;
+
+class SpyGram
+{
+    static void Main()
+    {
+        SortedDictionary<string, List<string>> messages =
+            new SortedDictionary<string, List<string>>();
+
+        string key = Console.ReadLine();
+
+        InsertMessage(messages);
+
+        PrintEncryptMessages(messages, key);
+    }
+
+    private static void InsertMessage(SortedDictionary<string, List<string>> messages)
+    {
+        while (true)
+        {
+            string readLine = Console.ReadLine();
+            if (readLine.Equals("END"))
+            {
+                break;
+            }
+
+            Match regex = Regex.Match(readLine,
+                @"^TO: (?<recipient>[A-Z]+); MESSAGE: (?<message>.+);$");
+
+            if (!regex.Success)
+            {
+                continue;
+            }
+
+            string recipient = regex.Groups["recipient"].Value;
+
+            if (!messages.ContainsKey(recipient))
+            {
+                messages.Add(recipient, new List<string>());
+            }
+
+            messages[recipient].Add(readLine);
+        }
+    }
+
+    private static void PrintEncryptMessages(SortedDictionary<string, List<string>> messages, string key)
+    {
+        foreach (var kvp in messages)
+        {
+            foreach (var m in kvp.Value)
+            {
+                StringBuilder encrypt = new StringBuilder();
+                for (int i = 0; i < m.Length; i++)
+                {
+                    int index = i % key.Length;
+                    char @char = (char)(m[i] + int.Parse(key[index].ToString()));
+                    encrypt.Append(@char.ToString());
+                }
+                Console.WriteLine($"{encrypt}");
+            }
+        }
+    }
+}
