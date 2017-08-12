ba00170c96c4e565fb9d95f6b40282f4494365f2
Peter Georgiev <peroxmen@hotmail.com>
2017-08-12 22:32:37 +0300

    Exam-09.July.2017_Part_1/01.PokeMon/PokeMon.cs - WORKS. Points: 100/100. Exam-09.July.2017_Part_1/02.PokemonDon'tGo/PokemonDontGo.cs - WORKS. Points: 100/100.

--- Exam-09.July.2017_Part_1/01.PokeMon/PokeMon.cs
+++ Exam-09.July.2017_Part_1/01.PokeMon/PokeMon.cs
@@ -1,14 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 
 class PokeMon
 {
     static void Main()
     {
+        int n = int.Parse(Console.ReadLine());
+        int m = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
 
+        decimal percentM = n * 0.5m;
+
+        int count = 0;
+        while (n >= m)
+        {
+            n -= m;
+            count++;
+
+            if (n == percentM && y > 0)
+            {
+                n /= y;
+            }
+        }
+
+        Console.WriteLine(n);
+        Console.WriteLine(count);
     }
 }
--- Exam-09.July.2017_Part_1/02.PokemonDon'tGo/PokemonDontGo.cs
+++ Exam-09.July.2017_Part_1/02.PokemonDon'tGo/PokemonDontGo.cs
@@ -1,50 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 class PokemonDontGo
 {
     static void Main()
     {
-        int n = int.Parse(Console.ReadLine());
-        int m = int.Parse(Console.ReadLine());
-        int y = int.Parse(Console.ReadLine());
+        long result = 0;
 
-        int originalN = n;
+        List<long> input = Console.ReadLine()
+            .Split(' ')
+            .Select(long.Parse)
+            .ToList();
 
-        int count = 0;
-        while (n > m)
+        while (input.Count > 0)
         {
-            n -= m;
-            count++;
-        }
-        
-        decimal percentM = ((long)m * (long)originalN) / 100m;
-        decimal percentN = ((long)originalN * (long)m) / 100m;
-
-        if (n < m)
-        {
-            Console.WriteLine(n);
-            Console.WriteLine(count);
-        }
-        else if (n == m)
-        {
-            if (n >= y && n != 0 && y != 0)
+            int command = int.Parse(Console.ReadLine());
+            
+            if (command < 0)
             {
-                int division = n / y;
-                Console.WriteLine(division);
+                long index = input[0];
+                input[0] = input[input.Count - 1];
+
+                GetIncreaseDecrease(input, index);
+                result += index;
+            }
+            else if (command > input.Count - 1)
+            {
+                long index = input[input.Count - 1];
+                input[input.Count - 1] = input[0];
+
+                GetIncreaseDecrease(input, index);
+                result += index;
+            }
+            else if (command < input.Count)
+            {
+                long index = input[command];
+                input.RemoveAt(command);
+
+                GetIncreaseDecrease(input, index);
+                result += index;
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+
+    static void GetIncreaseDecrease(List<long> input, long index)
+    {
+        for (int i = 0; i < input.Count; i++)
+        {
+            if (index >= input[i])
+            {
+                input[i] += index;
             }
             else
             {
-                while (n >= m)
-                {
-                    n -= m;
-                }
-                Console.WriteLine(n);
+                input[i] -= index;
             }
-            Console.WriteLine(count);
         }
     }
 }
