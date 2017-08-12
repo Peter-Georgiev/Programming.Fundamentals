8407a728ab46464d0843f4236c41071f345f799e
Peter Georgiev <peroxmen@hotmail.com>
2017-08-12 17:56:24 +0300

    Added Exam-09.May.2017.Part _II.sln. Exam-09.May.2017.Part _II/03.Spyfer/Spyfer.cs - WORKS. Points: 100/100. Exam-09.May.2017.Part _II/4.NSA/NSA.cs - WORKS. Points: 100/100.

---
+++ Exam-09.May.2017.Part _II/03.Spyfer/Spyfer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Spyfer
+{
+    static void Main()
+    {
+        List<int> input = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToList();
+
+        for (int i = 0; i < input.Count; i++)
+        {
+            int currentIndex = input[i];
+
+            if (i - 1 >= 0 && i + 1 < input.Count)
+            {
+                int sumNeighboring = input[i - 1] + input[i + 1];
+
+                if (sumNeighboring == currentIndex)
+                {
+                    input.RemoveAt(i + 1);
+                    input.RemoveAt(i - 1);
+                    i = 0;
+                }
+            }
+            else if (i + 1 < input.Count && currentIndex == input[i + 1])
+            {
+                input.RemoveAt(i + 1);
+                i = 0;
+            }
+            else if (i - 1 >= 0 && currentIndex == input[i - 1])
+            {
+                input.RemoveAt(i - 1);
+                i = 0;
+            }
+        }
+
+        Console.WriteLine(String.Join(" ", input));
+    }
+}
---
+++ Exam-09.May.2017.Part _II/4.NSA/NSA.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class NSA
+{
+    static void Main()
+    {
+        Dictionary<string, Dictionary<string, int>> nsa =
+            new Dictionary<string, Dictionary<string, int>>();
+
+        InsertNSA(nsa);
+
+        PrintResult(nsa);
+    }
+
+    private static void InsertNSA(Dictionary<string, Dictionary<string, int>> nsa)
+    {
+        while (true)
+        {
+            string readLine = Console.ReadLine();
+            if (readLine.Equals("quit"))
+            {
+                break;
+            }
+
+            string[] tokens = readLine
+                .Split(new string[] { " -> " },
+                StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            string country = tokens[0];
+            string spy = tokens[1];
+            int days = int.Parse(tokens[2]);
+
+            if (!nsa.ContainsKey(country))
+            {
+                nsa.Add(country, new Dictionary<string, int>());
+            }
+
+            if (!nsa[country].ContainsKey(spy))
+            {
+                nsa[country].Add(spy, 0);
+            }
+
+            nsa[country][spy] = days;
+        }
+    }
+
+    private static void PrintResult(Dictionary<string, Dictionary<string, int>> nsa)
+    {
+        foreach (var kvp in nsa.OrderByDescending(x => x.Value.Count))
+        {
+            Console.WriteLine($"Country: {kvp.Key}");
+
+            foreach (var s in kvp.Value.OrderByDescending(x => x.Value))
+            {
+                Console.WriteLine($"**{s.Key} : {s.Value}");
+            }
+        }
+    }
+}
