01ee65c0b1909c1df65281fcc19558ae281346f9
Peter Georgiev <peroxmen@hotmail.com>
2017-08-13 15:53:46 +0300

    Added Exam-09.July.2017.Part_2.sln. Added Exam-09.July.2017.Part_2/03. Regexmon_Условие.docx. Exam-09.July.2017.Part_2/03.Regexmon/Regexmon.cs - WORKS. Points: 100/100. Exam-09.July.2017.Part_2/04.PokemonEvolution/PokemonEvolution.cs - WORKS. Points: 100/100.

---
+++ Exam-09.July.2017.Part_2/03.Regexmon/Regexmon.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class Regexmon
+{
+    static void Main()
+    {
+        List<string> didimons = new List<string>();
+        List<string> bojomons = new List<string>();
+
+        string readLine = Console.ReadLine();
+
+        InsertDidimonAndBojomon(didimons, bojomons, readLine);
+
+        PrintResult(didimons, bojomons);
+    }
+
+    static void InsertDidimonAndBojomon(List<string> didimons, List<string> bojomons, string readLine)
+    {
+        bool hasDidimon = true;
+        bool hasBojomon = true;
+
+        while (true)
+        {
+            string pattern = @"(?<didimon>[^A-Za-z-]*)";
+            MatchCollection regex = Regex.Matches(readLine, pattern);  
+
+            foreach (Match r in regex)
+            {
+                string didimon = r.Groups["didimon"].Value;
+                if (didimon.Length > 0 && r.Success)
+                {
+                    didimons.Add(didimon);
+                    pattern = $@"(.*?{Regex.Escape(didimon)})?(.*)";
+                    readLine = Regex.Replace(readLine, pattern, @"$2");
+                    hasDidimon = false;
+                    break;
+                }
+            }
+
+            if (hasDidimon)
+            {
+                break;
+            }
+
+            pattern = @"(?<bojomon>[A-Za-z]+-[A-Za-z]+)";
+            regex = Regex.Matches(readLine, pattern);
+
+            foreach (Match r in regex)
+            {
+                string bojomon = r.Groups["bojomon"].Value;
+                if (bojomon.Length > 0 && r.Success)
+                {
+                    bojomons.Add(bojomon);
+                    pattern = $@"(.*?{Regex.Escape(bojomon)})?(.*)";
+                    readLine = Regex.Replace(readLine, pattern, @"$2");
+                    hasBojomon = false;
+                    break;
+                }
+            }
+
+            if (hasBojomon)
+            {
+                break;
+            }
+
+            hasDidimon = true;
+            hasBojomon = true;
+        }
+    }
+
+    static void PrintResult(List<string> didimons, List<string> bojomons)
+    {
+        for (int i = 0; i < Math.Max(didimons.Count, bojomons.Count); i++)
+        {
+            if (i < didimons.Count)
+            {
+                Console.WriteLine(didimons[i]);
+            }
+
+            if (i < bojomons.Count)
+            {
+                Console.WriteLine(bojomons[i]);
+            }
+        }
+    }
+}
---
+++ Exam-09.July.2017.Part_2/04.PokemonEvolution/PokemonEvolution.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Pokemons
+{
+    public string Type { get; set; }
+
+    public int Index { get; set; }
+}
+
+class PokemonEvolution
+{
+    static void Main()
+    {
+        Dictionary<string, List<Pokemons>> pokemons =
+            new Dictionary<string, List<Pokemons>>();
+
+        while (true)
+        {
+            string[] tokens = Console.ReadLine()
+            .Split(new string[] { " -> " },
+            StringSplitOptions.RemoveEmptyEntries)
+            .ToArray();
+
+            if (tokens[0].Equals("wubbalubbadubdub"))
+            {
+                break;
+            }
+            else if (tokens.Length == 3)
+            {
+                InsertPokemon(pokemons, tokens);
+            }
+            else if (pokemons.ContainsKey(tokens[0]))
+            {
+                GetPokemonName(pokemons, tokens[0]);
+            }
+        }
+
+        PrintResult(pokemons);
+    }
+
+    static void InsertPokemon(Dictionary<string, List<Pokemons>> pokemons, string[] tokens)
+    {
+        string name = tokens[0];
+        string type = tokens[1];
+        int index = int.Parse(tokens[2]);
+
+        if (!pokemons.ContainsKey(name))
+        {
+            pokemons.Add(name, new List<Pokemons>());
+        }
+
+        Pokemons newPokemon = new Pokemons()
+        {
+            Type = type,
+            Index = index
+        };
+
+        pokemons[name].Add(newPokemon);
+    }
+
+    static void GetPokemonName(Dictionary<string, List<Pokemons>> pokemons, string name)
+    {
+        foreach (var kvp in pokemons.Where(x => x.Key == name))
+        {
+            Console.WriteLine("# " + kvp.Key);
+
+            foreach (var e in kvp.Value)
+            {
+                Console.WriteLine(e.Type + " <-> " + e.Index);
+            }
+        }
+    }
+
+    static void PrintResult(Dictionary<string, List<Pokemons>> pokemons)
+    {
+        foreach (var kvp in pokemons)
+        {
+            Console.WriteLine("# " + kvp.Key);
+
+            foreach (var e in kvp.Value.OrderByDescending(x => x.Index))
+            {
+                Console.WriteLine(e.Type + " <-> " + e.Index);
+            }
+        }
+    }
+}
