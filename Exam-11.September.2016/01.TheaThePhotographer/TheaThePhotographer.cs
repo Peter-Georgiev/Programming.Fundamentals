b95918015a9c6e7173481d4012dd5abd24f73891
Peter Georgiev <peroxmen@hotmail.com>
2017-08-14 23:54:56 +0300

    Added Exam-11.September.2016.sln.

---
+++ Exam-11.September.2016/01.TheaThePhotographer/TheaThePhotographer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class TheaThePhotographer
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int ft = int.Parse(Console.ReadLine());
+        int ff = int.Parse(Console.ReadLine());
+        int ut = int.Parse(Console.ReadLine());
+
+        decimal useful = Math.Ceiling(n * (ff / 100m));
+        long totalTime = ((long)n * ft) + ((long)useful * ut);
+
+        TimeSpan t = TimeSpan.FromSeconds(totalTime);
+
+        string result = string.Format("{0}:{1:D2}:{2:D2}:{3:D2}",
+            t.Days,
+            t.Hours,
+            t.Minutes,
+            t.Seconds);
+
+        Console.WriteLine(result);
+    }
+}
---
+++ Exam-11.September.2016/02.TrophonTheGrumpyCat/TrophonTheGrumpyCat.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class TrophonTheGrumpyCat
+{
+    static void Main()
+    {
+        int[] arr = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+        int entryPoint = int.Parse(Console.ReadLine());
+        string typeOfItems = Console.ReadLine();
+        string typeOfPriceRating = Console.ReadLine();
+
+        if (typeOfItems.Equals("cheap"))
+        {
+         
+        }
+        else if (typeOfItems.Equals("expensive"))
+        {
+
+        }
+
+        if (typeOfPriceRating.Equals("positive"))
+        {
+
+        }
+        else if (typeOfPriceRating.Equals("negative"))
+        {
+
+        }
+        else if (typeOfPriceRating.Equals("all"))
+        {
+
+        }
+
+        string input;
+        while ((input = Console.ReadLine()) != "end")
+        {
+
+
+
+
+        }
+
+
+#if DEBUG
+        Console.WriteLine(4343);
+#endif
+    }
+
+    static int Cheap(int e)
+    {
+        throw new NotImplementedException();
+    }
+}
